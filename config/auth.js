b506bff1d9f4ec80aec96814bbf0632a0978eb6c
Carol Chen <carolyfisher@gmail.com>
2016-11-20 13:12:37 -0500

    Set up auth for deployment

--- config/auth.js
+++ config/auth.js
@@ -5,14 +5,14 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '699398166896313',
         'clientSecret'    : 'a74ccf862fe4adf5bd9f84cb8d0f5c5a',
-        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'     : 'http://pushme.gq/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
 
     },
     'googleAuth' : {
         'clientID'         : '221641113575-s90u8uugp84k23mec5db9gah9hhgvdd8.apps.googleusercontent.com',
         'clientSecret'     : 'kakdtj8VNh7zj1krYXzp0OfN',
-        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'      : 'http://pushme.gq/auth/google/callback'
     }
 
 };
